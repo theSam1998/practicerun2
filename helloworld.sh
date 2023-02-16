821517c66ee0bc5e88c1ae3346e0aa9d97a7805e
theSam1998 <2894sallan@gmail.com>
2023-02-16 01:23:11 +0000

    did it work this time?

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Sam Allan
+# Date of latest revision:2/15/2023
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+echo "I am working perfectly!"
+
+# End
