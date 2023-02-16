509ff2b5495691e3c5aeff0c8d47b94d3522253a
theSam1998 <2894sallan@gmail.com>
2023-02-16 00:39:25 +0000

    you got it, you understand now

--- practice.sh
+++ practice.sh
@@ -2,5 +2,10 @@
 
 # gonna practice this again to ensure i've got it right
 # any change made after line 4 is added in vscode
+# ok here comes changes
+# adding all these awesome changes
 
 # Main
+
+echo "you've done it, you get how to do this now"
+echo "sometimes stepping away is all it takes"
